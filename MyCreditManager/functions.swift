802531f2dd67a8ef32e82ba046dcf31d24abf1d4
zyeongPark <piiy10141997@gmail.com>
2022-11-27 23:11:21 +0900

    file add

---
+++ MyCreditManager/functions.swift
@@ -0,0 +1,8 @@
+//
+//  functions.swift
+//  MyCreditManager
+//
+//  Created by 박지영 on 2022/11/27.
+//
+
+import Foundation
--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -7,12 +7,12 @@
 
 import Foundation
 
+//git pull --no-rebase ## 깃헙 안될때
+
 //전체 구조 (크게 case 1,2,3,4,5,x로 나누고 각 기능을 함수로 나눠두자)
 
 //학생정보 저장용 딕셔너리
 var student: [String:[String]] = [:]
-
-//함수
 
 //전체 구조
 
