928b440d1f779021139ff912807ad743d70714ed
zyeongPark <piiy10141997@gmail.com>
2023-01-09 11:57:15 +0900

    case문 수정

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -21,9 +21,15 @@ var input = readLine()!
 while true{
     switch input{
     case "1":
-        print("a")
+        print("학생 추가")
     case "2":
-        print("b")
+        print("학생 삭제")
+    case "3":
+        print("성적추가(변경)")
+    case "4":
+        print("성적삭제")
+    case "5":
+        print("평점보기")
     default:
         print("다시 입력하세요")
     }
